3e70d595d5da98311d71d559efc1c90a8aa4fdbd
Austin Hedeen <austin.hedeen@gmail.com>
2019-09-09 13:10:08 -0700

    Hook command route to bot message

--- src/routes/commands.js
+++ src/routes/commands.js
@@ -3,9 +3,12 @@
 const express = require('express');
 const router = express.Router();
 
+const slackbot = require('../slackbot.js');
+
 router.post('/slack/testcommand', (request, response) => {
   // TODO -- Send receipt to Slack
   console.log('Request from Slack');
+  slackbot('command-testing', 'Hooray, it worked!');
   response.status(200).send();
 });
 
